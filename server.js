e3efb8ad612a231d1625a157ec323befb58e6b33
Adriaan350 <vanderheijdenadriaan@gmail.com>
2021-03-09 01:13:44 +0100

    final final

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const session = require('express-session');
 const app = express();
-const port = 8000
+const port = process.env.PORT || 8080
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
